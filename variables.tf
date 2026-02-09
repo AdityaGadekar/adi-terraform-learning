b756c0d719d6f6ae2d97cb06d3aeb121ec7ff6e3
AdityaGadekar <60725722+AdityaGadekar@users.noreply.github.com>
2026-02-09 12:51:50 +0530

    Add variables for file name and content

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "file_name" {
+  description = "File name from Terraform Cloud variable"
+  type        = string
+}
+
+variable "file_content" {
+  description = "File content from Terraform Cloud variable"
+  type        = string
+}
